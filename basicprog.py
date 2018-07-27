aba02d4b011fac10d67719b256f5f553aac48d2e
sreedhar9999 <41770029+sreedhar9999@users.noreply.github.com>
2018-07-27 01:38:42 -0700

    Update and rename firstprog.py to basicprog.py

---
+++ basicprog.py
@@ -0,0 +1,7 @@
+a=int(input('INPUT'))
+if(a>0):
+   print("it is Positive")
+elif(a==0):
+   print("it is Neutral")
+   else:
+      print("it is Negative")
--- firstprog.py
+++
@@ -1,7 +0,0 @@
-a=raw_input()
-if(int(a)<0):
-   print("it is negative")
-elif(int(a)==0):
-   print("it is neutral")
-else:
-   print("it is positive")
