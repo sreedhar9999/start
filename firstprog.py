ff2e97ca45a379aaafecad56764e7159e2713f37
sreedhar9999 <41770029+sreedhar9999@users.noreply.github.com>
2018-07-27 00:22:01 -0700

    Update and rename sample.py to firstprog.py

---
+++ firstprog.py
@@ -0,0 +1,7 @@
+a=raw_input()
+if(int(a)<0):
+  print("it is negative")
+  elif(int(a)==0)
+  print("it is neutral")
+  else:
+    print("it is positive")
--- sample.py
+++
@@ -1,1 +0,0 @@
-mhfdakjhsavrliwnvetgyuilrngffhfh
